7ed4544d87686ef5295362943602bc81665da484
saumya <2saumyaray@gmail.com>
2021-02-09 19:19:12 +0530

    Experementing with Expo. React-Native.

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text> Expo ReactNative </Text>
+      <Text> Expo Experiment. </Text>
       <StatusBar style="auto" />
     </View>
   );
