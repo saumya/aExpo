20b3f047fd13de008fd3f3988ca6db6790905a3f
saumya <2saumyaray@gmail.com>
2021-02-14 21:57:26 +0530

    wip: trying components from Expo

--- App.js
+++ App.js
@@ -2,11 +2,14 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import HomeComp from './comps/Home.comp';
+
 export default function App() {
   return (
     <View style={styles.container}>
       <Text> Expo Experiment. </Text>
       <StatusBar style="auto" />
+      <HomeComp />
     </View>
   );
 }
